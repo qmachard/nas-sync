7b2026e7aec456bda94421c5c1caabc07d7c5270
Quentin Machard <quentin.machard@gmail.com>
2016-03-28 18:51:18 +0200

    Don't sync couchpotato folder

--- sync-movies.sh
+++ sync-movies.sh
@@ -4,6 +4,6 @@
 
 echo "\n######## Starting sync movies\n" >> ./log/movies.log
 
-rsync -azv "${SEEDBOX_USER}"@"${SEEDBOX_HOST}":"${SEEDBOX_PATH_MOVIES}" "${LOCAL_PATH_MOVIES}" >> ./log/movies.log
+rsync -azv --exclude "couchpotato" "${SEEDBOX_USER}"@"${SEEDBOX_HOST}":"${SEEDBOX_PATH_MOVIES}" "${LOCAL_PATH_MOVIES}" >> ./log/movies.log
 
 echo "\n######## Ending sync movies\n" >> ./log/movies.log
