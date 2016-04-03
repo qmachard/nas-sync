7551cdf7986afef5281ef5551d4f7d61ddff8dbf
Quentin Machard <quentin.machard@gmail.com>
2016-04-03 19:53:17 +0200

    Remove progress bar

--- sync-movies.sh
+++ sync-movies.sh
@@ -18,7 +18,7 @@ fi
 
 echo "\n######## Starting sync movies\n"
 
-rsync -azv --progress --exclude "couchpotato" "${SEEDBOX_USER}"@"${SEEDBOX_HOST}":"${SEEDBOX_PATH_MOVIES}" "${LOCAL_PATH_MOVIES}"
+rsync -azv --exclude "couchpotato" "${SEEDBOX_USER}"@"${SEEDBOX_HOST}":"${SEEDBOX_PATH_MOVIES}" "${LOCAL_PATH_MOVIES}"
 
 echo "\n######## Ending sync movies\n"
 
